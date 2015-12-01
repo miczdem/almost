f4e361faa047cbfc793368e24936ba782ad77046
Michael Demarco <miczdem@gmail.com>
2015-12-01 22:48:31 +1100

    added thinktecture user manager following https://vimeo.com/125426951

--- Mic.Almost.WebUI/Startup.cs
+++ Mic.Almost.WebUI/Startup.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Owin;
+﻿using IdentityManager.Configuration;
+using IdentityManager.AspNetIdentity;
+using IdentityManager;
+using Mic.Almost.WebUI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartupAttribute(typeof(Mic.Almost.WebUI.Startup))]
@@ -9,6 +15,53 @@ namespace Mic.Almost.WebUI
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+
+            app.Map("/idm", idm =>
+            {
+                var factory = new IdentityManagerServiceFactory();
+                factory.IdentityManagerService = new Registration<IIdentityManagerService,ApplicationIdentityManagerService>();
+                factory.Register(new Registration<ApplicationUserManager>());
+                factory.Register(new Registration<ApplicationUserStore>());
+                factory.Register(new Registration<ApplicationRoleManager>());
+                factory.Register(new Registration<ApplicationRoleStore>());
+                factory.Register(new Registration<ApplicationDbContext>());
+
+                idm.UseIdentityManager(new IdentityManagerOptions {
+                    Factory = factory
+                });            
+            });
+        }
+    }
+
+
+    public class ApplicationUserStore : UserStore<ApplicationUser>
+    {
+        public ApplicationUserStore(ApplicationDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+    public class ApplicationRoleStore : RoleStore<IdentityRole>
+    {
+        public ApplicationRoleStore(ApplicationDbContext dbContext):base(dbContext)
+        {
+            
+        }       
+    }
+
+    public class ApplicationRoleManager : RoleManager<IdentityRole>
+    {
+        public ApplicationRoleManager(ApplicationRoleStore roleStore):base(roleStore)
+        {
+            
+        }
+    }
+
+    public class ApplicationIdentityManagerService : AspNetIdentityManagerService<ApplicationUser, string, IdentityRole, string>
+    {
+        public ApplicationIdentityManagerService(ApplicationUserManager userMgr, ApplicationRoleManager roleMgr) : base(userMgr,roleMgr)
+        {
+            
         }
     }
 }
